842d94c578f69e753e9ff4e229e1b469d6e0673a
Justin Shacklette <justin@saturnboy.com>
2015-01-06 14:10:35 -0700

    add missing doc to is_unicode()

--- assertpy/assertpy.py
+++ assertpy/assertpy.py
@@ -296,6 +296,7 @@ class AssertionBuilder(object):
         return self
 
     def is_unicode(self):
+        """Asserts that val is a unicode string."""
         if type(self.val) is not unicode:
             raise AssertionError('Expected <%s> to be unicode, but was <%s>.' % (self.val, type(self.val).__name__))
         return self
