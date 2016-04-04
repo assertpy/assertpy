78114e63206ec6cad438407a0ed0572146257ec5
Mario Campos <mario@mariocampos.io>
2016-04-04 11:08:43 -0500

    should fix py3 is_subset_of test cases

--- tests/test_set.py
+++ tests/test_set.py
@@ -24,7 +24,7 @@ class TestSet(object):
             assert_that(set([1,2,3])).is_subset_of(set())
             fail('should have raised error')
         except AssertionError as ex:
-            assert_that(str(ex)).is_equal_to('Expected <set([1, 2, 3])> to be a subset of <set([])>, but was not.')
+            assert_that(str(ex)).is_equal_to('Expected <%s> to be a subset of <%s>, but was not.' % (set([1,2,3]), set()))
 
     def test_positive_normal_case(self):
         assert_that(set([1])).is_subset_of(set([1,2,3]))
@@ -34,5 +34,5 @@ class TestSet(object):
             assert_that(set([1,2,3])).is_subset_of(set([1]))
             fail('should have raised error')
         except AssertionError as ex:
-            assert_that(str(ex)).is_equal_to('Expected <set([1, 2, 3])> to be a subset of <set([1])>, but was not.')
+            assert_that(str(ex)).is_equal_to('Expected <%s> to be a subset of <%s>, but was not.' % (set([1,2,3]), set([1])))
 
