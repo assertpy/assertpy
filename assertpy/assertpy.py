9df9ca36d8b2603d5d170a6531dc870bd05068d1
Justin Shacklette <justin@saturnboy.com>
2017-01-08 11:37:29 -0700

    force fail to always fail immediately, see #58

--- assertpy/assertpy.py
+++ assertpy/assertpy.py
@@ -59,19 +59,23 @@ def soft_assertions():
     global _soft_ctx
     global _soft_err
 
+    # init ctx
     _soft_ctx = True
     _soft_err = []
 
-    yield
+    try:
+        yield
+    finally:
+        # reset ctx
+        _soft_ctx = False
 
     if _soft_err:
         out = 'soft assertion failures:'
         for i,msg in enumerate(_soft_err):
             out += '\n%d. %s' % (i+1, msg)
+        # reset msg, then raise
+        _soft_err = []
         raise AssertionError(out)
-
-    _soft_err = []
-    _soft_ctx = False
 
 
 ### factory methods ###
@@ -121,10 +125,7 @@ def contents_of(f, encoding='utf-8'):
 
 def fail(msg=''):
     """Force test failure with the given message."""
-    if len(msg) == 0:
-        raise AssertionError('Fail!')
-    else:
-        raise AssertionError('Fail: %s!' % msg)
+    raise AssertionError('Fail: %s!' % msg if msg else 'Fail!')
 
 
 class AssertionBuilder(object):
