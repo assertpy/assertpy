f0bd9e49a40a988b46e7e7f15979eabf89e5f5dd
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2017-01-09 14:39:30 +0300

    add soft_fail() and tests

--- assertpy/assertpy.py
+++ assertpy/assertpy.py
@@ -127,6 +127,16 @@ def fail(msg=''):
     """Force test failure with the given message."""
     raise AssertionError('Fail: %s!' % msg if msg else 'Fail!')
 
+def soft_fail(msg=''):
+    """Adds error message to soft errors list if within soft assertions context.
+       Either just force test failure with the given message."""
+    global _soft_ctx
+    if _soft_ctx:
+        global _soft_err
+        _soft_err.append('Fail: %s!' % msg if msg else 'Fail!')
+        return
+    fail(msg)
+
 
 class AssertionBuilder(object):
     """Assertion builder."""
