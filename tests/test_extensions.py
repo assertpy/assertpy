0c754547c6b75aa5d8519cfb932c91e82e53166d
Justin Shacklette <justin@saturnboy.com>
2019-10-21 20:04:39 -0600

    refactor to add extensions just once

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -47,16 +47,16 @@ def is_multiple_of(self, other):
         self._err('Expected <%s> to be multiple of <%s>, but was not.' % (self.val, other))
     return self
 
+add_extension(is_even)
+add_extension(is_multiple_of)
 
 
 def test_is_even_extension():
-    add_extension(is_even)
     assert_that(124).is_even()
     assert_that(124).is_type_of(int).is_even().is_greater_than(123).is_less_than(125).is_equal_to(124)
 
 def test_is_even_extension_failure():
     try:
-        add_extension(is_even)
         assert_that(123).is_even()
         fail('should have raised error')
     except AssertionError as ex:
@@ -71,14 +71,12 @@ def test_is_even_extension_failure_not_callable():
 
 def test_is_even_extension_failure_not_integer():
     try:
-        add_extension(is_even)
         assert_that(124.0).is_even()
         fail('should have raised error')
     except TypeError as ex:
         assert_that(str(ex)).is_equal_to('val must be an integer')
 
 def test_is_multiple_of_extension():
-    add_extension(is_multiple_of)
     assert_that(24).is_multiple_of(1)
     assert_that(24).is_multiple_of(2)
     assert_that(24).is_multiple_of(3)
@@ -91,7 +89,6 @@ def test_is_multiple_of_extension():
 
 def test_is_multiple_of_extension_failure():
     try:
-        add_extension(is_multiple_of)
         assert_that(24).is_multiple_of(5)
         fail('should have raised error')
     except AssertionError as ex:
@@ -99,7 +96,6 @@ def test_is_multiple_of_extension_failure():
 
 def test_is_multiple_of_extension_failure_not_integer():
     try:
-        add_extension(is_multiple_of)
         assert_that(24.0).is_multiple_of(5)
         fail('should have raised error')
     except TypeError as ex:
@@ -107,7 +103,6 @@ def test_is_multiple_of_extension_failure_not_integer():
 
 def test_is_multiple_of_extension_failure_arg_not_integer():
     try:
-        add_extension(is_multiple_of)
         assert_that(24).is_multiple_of('foo')
         fail('should have raised error')
     except TypeError as ex:
